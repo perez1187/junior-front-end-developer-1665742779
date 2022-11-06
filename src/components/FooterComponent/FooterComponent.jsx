4cf32b7190d6b55c2b69513819e33f670625068f
perez1187 <o.perez1187@gmail.com>
2022-11-06 22:13:45 +0100

    footer

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -6,19 +6,35 @@ import './FooterComponent.css'
 function FooterComponent() {
   return (
     <div className='Footer'>
-        <div className='FooterComponentLeft'> 
-          <div className='FooterTermsOfService'> Terms of service</div>
-          <div className='FooterDot'>•</div>
-          <div className='FooterPrivacyPolicy'>Privacy policy</div>
-          <div className='FooterDot2'>•</div>
-          <div className='FooterCopyright'>Copyright</div>
-        </div>
-        <div className='FooterComponentRight'>
-           <div className='FooterNerdsFamilyVersion'>nerds.family Version 1.2</div>
+      <div className='FooterComponentLeft'> 
+        <div className='FooterTermsOfService'> Terms of service</div>
+        <div className='FooterDot'>•</div>
+        <div className='FooterPrivacyPolicy'>Privacy policy</div>
+        <div className='FooterDot2'>•</div>
+        <div className='FooterCopyright'>Copyright</div>
+      </div>
+      <div className='FooterComponentRight'> 
+          <div className='FooterNerdsFamilyVersion'>nerds.family Version 1.2</div>
            <div className='FooterDot3'>•</div>
            <div className='FooterLastUpdate'>Last update 10/09/2022</div>
-        </div>
+      </div>
     </div>
+    
+    // <div className='Footer'>
+    //     <div className='FooterComponentLeft'> 
+    //       <div className='FooterTermsOfService'> Terms of service</div>
+    //       <div className='FooterDot'>•</div>
+    //       <div className='FooterPrivacyPolicy'>Privacy policy</div>
+    //       <div className='FooterDot2'>•</div>
+    //       <div className='FooterCopyright'>Copyright</div>
+    //     </div>
+    //     <div className='FooterComponentRight'>
+          //  <div className='FooterNerdsFamilyVersion'>nerds.family Version 1.2</div>
+          //  <div className='FooterDot3'>•</div>
+          //  <div className='FooterLastUpdate'>Last update 10/09/2022</div>
+    //     </div>
+    // </div>
+
   )
 }
 
--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -3,16 +3,25 @@ import React from 'react'
 // css
 import './NavbarComponent.css'
 
-// components
-import NavbarComponentAvatar from './NavbarComponentAvatar'
-import NavbarComponentLogo from './NavbarComponentLogo'
+// logo
+import logo from './../../assets/Logo/logoCzarne.png'
+
+// manual avatar image
+import userAvatar from '../../assets/MockedData/UserAvatar.png'
+
 
 function NavbarComponent() {
 
   return (
     <div className='Menu'>
-        <NavbarComponentLogo/>
-        <NavbarComponentAvatar/>
+
+      <div> 
+        <img src={logo} alt="Logo" className='LogoNavbar'/>
+      </div>
+      <div className='UserImageNavbar'>
+        <div className='AvatarNavbar' style={{backgroundImage: `url(${userAvatar})`}}></div>
+      </div>
+      
     </div>
   )
 }
